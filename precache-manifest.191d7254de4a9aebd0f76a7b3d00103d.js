46d19290fc050b18a258b89bc65465966f788ca5
Dan Horne <d.horne92@live.co.uk>
2020-05-20 12:41:36 +0100

    Updates

---
+++ precache-manifest.191d7254de4a9aebd0f76a7b3d00103d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352dfe963734867a453b180b3a4eb0a2",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "6987c7fdb8a15f55de95",
+    "url": "/weatherapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f0616e000bc933b2233b",
+    "url": "/weatherapp/static/js/2.d23b8dd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherapp/static/js/2.d23b8dd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6987c7fdb8a15f55de95",
+    "url": "/weatherapp/static/js/main.438b2268.chunk.js"
+  },
+  {
+    "revision": "3652742f28b1fb0df22c",
+    "url": "/weatherapp/static/js/runtime-main.ed694703.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weatherapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
